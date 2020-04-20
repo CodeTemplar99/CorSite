620f5554ece13da627e3645bd5f3072e1321229c
Chiadika <udoagwac@gmail.com>
2020-04-20 23:58:30 +0100

    slides, partners

--- js/spine.js
+++ js/spine.js
@@ -6,3 +6,16 @@ function showSideBar() {
     isOpen = !isOpen
 
 }
+
+
+// slide
+var on = true;
+var myVar = setInterval(function () { if (on) { slides() } on = true; }, 7000);
+
+function slides() {
+    document.getElementsByTagName("img")[0].style.width = "0px";
+    document.getElementsByTagName("img")[1].style.width = "100%";
+    var slide = document.getElementsByTagName("img")[0];
+    document.getElementById("slide").appendChild(slide);
+
+}
